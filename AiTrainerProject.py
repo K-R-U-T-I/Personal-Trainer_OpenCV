235071a08c43a815231a249bc3680f11f8ef340b
K-R-U-T-I <krutimittal3303@gmail.com>
2021-05-16 19:10:06 +0530

    Personal AI Trainer using OpenCV!

---
+++ AiTrainerProject.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+import mediapipe
+import time
+import PoseModule as pm
+
+cap = cv2.VideoCapture('Videos/curls.mp4')
+
+detector = pm.PoseDetector()
+count = 0
+dir = 0
+pTime = 0
+
+while True:
+    success, img = cap.read()
+    img = cv2.resize(img, (1280,720))
+    # img = cv2.imread('Videos/test.jpg')
+    img = detector.findPose(img, False)
+    lmList = detector.findPosition(img, False)
+
+    if len(lmList) != 0:
+        # detector.findAngle(img,12,14,16)    # Right Arm
+        angle = detector.findAngle(img,11,13,15)    # Left Arm
+        # detector.findAngle(img,23,25,27)    # Left Leg
+        # detector.findAngle(img,24,26,28)    # Right Leg
+
+        per = np.interp(angle, (210,310), (0,100))
+        bar = np.interp(angle, (220,310), (650,100))
+        # print(angle, per)
+
+        # Check for the dumbbell curls
+        color = (255,0,255)
+        if per == 100:
+            color = (0,255,0)
+            if dir == 0:
+                count += 0.5
+                dir = 1
+        if per == 0:
+            color = (0,0,255)
+            if dir == 1:
+                count += 0.5
+                dir = 0
+
+        # Draw Bar
+        cv2.rectangle(img, (1100,100), (1175,650), color, 3)
+        cv2.rectangle(img, (1100,int(bar)), (1175,650), color, cv2.FILLED)
+        cv2.putText(img, f'{int(per)} %', (1100,75), cv2.FONT_HERSHEY_PLAIN,
+                    4, color, 4)
+
+        # Draw Curl Count
+        cv2.rectangle(img, (0,450), (250,720), (0,255,0), cv2.FILLED)
+        cv2.putText(img, str(int(count)), (45,670), cv2.FONT_HERSHEY_PLAIN,
+                    15, (255,0,0), 30)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (50,100), cv2.FONT_HERSHEY_PLAIN,
+                5, (255,0,0), 5)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
